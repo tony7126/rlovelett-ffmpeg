62a17d22f4549c46243a1d929528ed16123404e6
Ryan Lovelett <ryan@lovelett.me>
2014-02-07 14:50:01 -0500

    Deprecate all convenience methods on Movie
    All convenience methods on Movie that are just pointers to methods on
    the first VideoStream or AudioStream have been marked for deprecation.

--- lib/ffmpeg/movie.rb
+++ lib/ffmpeg/movie.rb
@@ -75,6 +75,7 @@ module FFMPEG
     # This is really just a convenience method to AudioStream#to_s
     # @return [String?] A string description of the first audio stream
     def audio_stream
+      warn '[DEPRECATION] `Movie#audio_channels` is deprecated. Please start looking at `Movie#audio_streams`'
       audio_streams.first.to_s.match(/Audio:\s+(.*)$/)[1] unless @invalid or audio_streams.empty?
     end
 
@@ -85,6 +86,7 @@ module FFMPEG
     # This is really just a convenience method to AudioStream#codec_name
     # @return [String?] The audio codec of the first audio stream.
     def audio_codec
+      warn '[DEPRECATION] `Movie#audio_codec` is deprecated. Please start looking at `Movie#audio_streams`'
       audio_streams.first.codec_name unless @invalid or audio_streams.empty?
     end
 
@@ -94,6 +96,7 @@ module FFMPEG
     # This is really just a convenience method to AudioStream#bitrate
     # @return [Fixnum?] The bit rate of the first audio stream
     def audio_bitrate
+      warn '[DEPRECATION] `Movie#audio_bitrate` is deprecated. Please start looking at `Movie#audio_streams`'
       audio_streams.first.bitrate
     end
 
@@ -102,6 +105,7 @@ module FFMPEG
     # This is really just a convenience method to AudioStream#sample_rate
     # @return [Integer?] The audio codec sample rate in Hertz
     def audio_sample_rate
+      warn '[DEPRECATION] `Movie#audio_sample_rate` is deprecated. Please start looking at `Movie#audio_streams`'
       audio_streams.first.sample_rate unless @invalid or audio_streams.empty?
     end
 
@@ -111,6 +115,7 @@ module FFMPEG
     # This is really just a convenience method to AudioStream#channels
     # @return [Integer?] The number of audio channels of the first audio stream
     def audio_channels
+      warn '[DEPRECATION] `Movie#audio_channels` is deprecated. Please start looking at `Movie#audio_streams`'
       audio_streams.first.channels unless @invalid or audio_streams.empty?
     end
 
@@ -121,6 +126,7 @@ module FFMPEG
     # This is really just a convenience method to VideoStream#to_s
     # @return [String?] A string description of the first video stream
     def video_stream
+      warn '[DEPRECATION] `Movie#video_stream` is deprecated. Please start looking at `Movie#video_streams`'
       video_streams.first.to_s.match(/Video:\s+(.*)$/)[1] unless @invalid or video_streams.empty?
     end
 
@@ -130,6 +136,7 @@ module FFMPEG
     # This is really just a convenience method to VideoStream#codec_name
     # @return [String?] The name of the codec for the first video stream
     def video_codec
+      warn '[DEPRECATION] `Movie#video_codec` is deprecated. Please start looking at `Movie#video_streams`'
       video_streams.first.codec_name unless @invalid or video_streams.empty?
     end
 
@@ -138,6 +145,7 @@ module FFMPEG
     # This is really just a convenience method to VideoStream#video_bitrate
     # @return [Fixnum?] The bit rate of the first video stream
     def video_bitrate
+      warn '[DEPRECATION] `Movie#video_bitrate` is deprecated. Please start looking at `Movie#video_streams`'
       video_streams.first.bitrate unless @invalid or video_streams.empty?
     end
 
@@ -146,6 +154,7 @@ module FFMPEG
     # This is really just a convenience method to VideoStream#colorspace
     # @return [String?] The pixel format of the first video stream
     def colorspace
+      warn '[DEPRECATION] `Movie#colorspace` is deprecated. Please start looking at `Movie#video_streams`'
       video_streams.first.colorspace unless @invalid or video_streams.empty?
     end
 
@@ -153,6 +162,7 @@ module FFMPEG
     #
     # This is really just a convenience method to VideoStream#width
     def width
+      warn '[DEPRECATION] `Movie#width` is deprecated. Please start looking at `Movie#video_streams`'
       video_streams.first.width unless @invalid or video_streams.empty?
     end
 
@@ -160,6 +170,7 @@ module FFMPEG
     #
     # This is really just a convenience method to VideoStream#height
     def height
+      warn '[DEPRECATION] `Movie#height` is deprecated. Please start looking at `Movie#video_streams`'
       video_streams.first.height unless @invalid or video_streams.empty?
     end
 
@@ -167,6 +178,7 @@ module FFMPEG
     #
     # This is really just a convenience method to VideoStream#sar
     def sar
+      warn '[DEPRECATION] `Movie#sar` is deprecated. Please start looking at `Movie#video_streams`'
       video_streams.first.sar unless @invalid or video_streams.empty?
     end
 
@@ -174,6 +186,7 @@ module FFMPEG
     #
     # This is really just a convenience method to VideoStream#dar
     def dar
+      warn '[DEPRECATION] `Movie#dar` is deprecated. Please start looking at `Movie#video_streams`'
       video_streams.first.dar unless @invalid or video_streams.empty?
     end
 
@@ -181,6 +194,7 @@ module FFMPEG
     #
     # This is really just a convenience method to VideoStream#avg_frame_rate
     def frame_rate
+      warn '[DEPRECATION] `Movie#frame_rate` is deprecated. Please start looking at `Movie#video_streams`'
       video_streams.first.avg_frame_rate unless @invalid or video_streams.empty?
     end
 
@@ -188,6 +202,7 @@ module FFMPEG
     #
     # This is really just a convenience method to VideoStream#rotation
     def rotation
+      warn '[DEPRECATION] `Movie#rotation` is deprecated. Please start looking at `Movie#video_streams`'
       video_streams.first.rotation unless @invalid or video_streams.empty?
     end
 
@@ -196,16 +211,19 @@ module FFMPEG
     end
 
     def resolution
+      warn '[DEPRECATION] `Movie#resolution` is deprecated. Please start looking at `Movie#video_streams`'
       unless width.nil? or height.nil?
         "#{width}x#{height}"
       end
     end
 
     def calculated_aspect_ratio
+      warn '[DEPRECATION] `Movie#calculated_aspect_ratio` is deprecated. Please start looking at `Movie#video_streams`'
       video_streams.first.calculated_aspect_ratio unless @invalid or video_streams.empty?
     end
 
     def calculated_pixel_aspect_ratio
+      warn '[DEPRECATION] `Movie#calculated_pixel_aspect_ratio` is deprecated. Please start looking at `Movie#video_streams`'
       video_streams.first.calculated_pixel_aspect_ratio unless @invalid or video_streams.empty?
     end
 
